f7944e92d3fc7ef74a650aa09fe8567ce25485bf
giolongo <longo@polarisengineeringspa.com>
2024-08-12 19:50:12 +0200

    created Product Model and Factory

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Product extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name', 
+        'description', 
+        'price', 
+        'promo_price', 
+        'in_promo', 
+        'quantity'
+    ];
+}
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->text(),
+            'in_promo' => false,
+            'promo_price' => null,
+            'price' => fake()->numberBetween(100, 1000),
+            'quantity' => fake()->numberBetween(1, 100),
+        ];
+
+    }
+
+    public function inPromo(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'in_promo' => true,
+                'promo_price' => fake()->numberBetween(100, $attributes['price'] - 10),
+            ];
+        });
+    }
+}
